e1031dde48b55b88f248b4ddfb09a5de94eca8c8
MalicknND <ndiayemalick062@gmail.com>
2025-12-16 16:37:19 +0100

    Ajout du code pour manipuler un dictionnaire de fruits dans l'exercice P1C7

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,26 @@
 # Écrivez votre code ici !
+
+fruits = {
+    "pomme": "rouge",
+    "banane": "jaune",
+    "orange": "orange"
+}
+
+# Ajout de la clé "kiwi" avec la valeur "vert"
+fruits["kiwi"] = "vert"
+print(fruits)
+
+# Accès à la valeur correspondant à la clé "banane"
+couleur_banane = fruits["banane"]
+print(couleur_banane)
+
+# Modification de la valeur associée à la clé "pomme"
+fruits["pomme"] = "vert"
+print(fruits)
+
+# Suppression de la clé "banane"
+del fruits["banane"]
+print(fruits)
+
+# Affichage des clés restantes dans le dictionnaire
+print(fruits.keys())
