5db988a34f296144e331b8a30dbfa1162a7aff4a
MalicknND <ndiayemalick062@gmail.com>
2025-12-16 15:48:10 +0100

    Ajout des exemples de code pour les exercices P1C3 et P1C4

--- P1/P1C3/énoncé/main.py
+++ P1/P1C3/énoncé/main.py
@@ -1,1 +1,4 @@
-## Écrivez votre code ici !
+# Écrivez votre code ici !
+print("J'apprends le langage Python.")
+operation = (17 + 35) * 2
+print("Le résultat de l'opération (17 + 35) * 2 est :", operation)
--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,7 @@
 # Écrivez votre code ici !
+nom = "NDIAYE"
+prenom = "Malick"
+age = 25
+print(f"Bonjour, je m'appelle {prenom} {nom} et j'ai {age} ans.")
+age += 10
+print(f"Bonjour, je m'appelle {prenom} {nom} et j'ai maintenant {age} ans.")
