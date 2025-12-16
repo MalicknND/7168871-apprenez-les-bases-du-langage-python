202e91a8b976d569ffda81fd78c2eb99ea0d5392
MalicknND <ndiayemalick062@gmail.com>
2025-12-16 16:01:08 +0100

    Ajout du code pour les variables et les types dans l'exercice P1C5

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,14 @@
 # Écrivez votre code ici !
+nom = "Malick"
+age = 25
+taille = 1.75
+est_etudiant = True
+
+print("Nom :", nom)
+print("Âge :", age)
+print("Taille :", taille)
+print("Est étudiant :", est_etudiant)
+print("Type de 'nom' :", type(nom))
+print("Type de 'age' :", type(age))
+print("Type de 'taille' :", type(taille))
+print("Type de 'est_etudiant' :", type(est_etudiant))
