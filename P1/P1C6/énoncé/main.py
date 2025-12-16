26ad7a797a0c28ec15ffc38778ca57ceb9b535d5
MalicknND <ndiayemalick062@gmail.com>
2025-12-16 16:24:14 +0100

    Ajout du code pour manipuler une liste de fruits dans l'exercice P1C6

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,11 @@
 # Écrivez votre code ici !
+fruits = ["pomme", "banane", "orange"]
+fruits.append("kiwi")
+print(fruits)
+del fruits[2]
+print(fruits)
+fruits[1] = "ananas"
+print(fruits)
+print(len(fruits))
+fruits.sort()
+print(fruits)
