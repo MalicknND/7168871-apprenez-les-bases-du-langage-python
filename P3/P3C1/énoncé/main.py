666a9cf9e79afeb6ecd0663b3a3b99e207f38e14
MalicknND <ndiayemalick062@gmail.com>
2025-12-17 00:49:49 +0100

    Ajout du code pour effectuer des opérations d'addition et de multiplication sur deux nombres saisis par l'utilisateur

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,11 @@
 # Ecrivez votre code ici !
+from operations import addition, multiplication
+
+num1 = float(input("Entrez le premier nombre : "))
+num2 = float(input("Entrez le deuxième nombre : "))
+
+somme = addition(num1, num2)
+produit = multiplication(num1, num2)
+
+print(f"La somme de {num1} et {num2} est : {somme}")
+print(f"Le produit de {num1} et {num2} est : {produit}")
